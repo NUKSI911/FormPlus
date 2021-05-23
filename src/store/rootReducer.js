db65c580eb4badff32d757a4f55af318a845d089
nurudeen yekeen <nurudeeny@HARDCOREBIOMETRIC.COM>
2021-05-23 22:06:15 +0100

    format:  formatted codebase with prettier

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,19 +1,18 @@
-import {  combineReducers } from "redux";
-import templateReducer from '../pages/Template/Store/template.reducer'
+import { combineReducers } from "redux";
+import templateReducer from "../pages/Template/Store/template.reducer";
 
 const appReducer = combineReducers({
   template: templateReducer,
 });
 
-function rootReducer(state={}, action) {
-
+function rootReducer(state = {}, action) {
   if (action.type === "RESET") {
     state = {
       ...state,
       template: undefined,
     };
-}
-return appReducer(state, action);
+  }
+  return appReducer(state, action);
 }
 
 export default rootReducer;
